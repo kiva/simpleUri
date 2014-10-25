32ea8677f18f5607ecd2f1a1765e8ac6eeb387c8
uglymunky <gabriel@kiva.org>
2014-10-25 16:23:20 -0700

    noe

--- test/buster.js
+++ test/buster.js
@@ -1,17 +1,17 @@
 var config = module.exports;
 
 config['development'] = {
-	environment: 'browser'
+	env: 'browser'
     , rootPath: '../'
     , deps: [
         'bower_components/jquery/jquery.js'
     ]
     , sources: ['src/simpleUri.js']
     , specs: ['test/spec/**/*.js']
-    , extensions: [ require('buster-coverage') ]
-    , "buster-coverage": {
-        outputDirectory: "test/coverage"
-        , format: "lcov"
-        , combinedResultsOnly: true
-    }
+    //, extensions: [ require('buster-coverage') ]
+    //, "buster-coverage": {
+    //    outputDirectory: "test/coverage"
+    //    , format: "lcov"
+    //    , combinedResultsOnly: true
+    //}
 };
